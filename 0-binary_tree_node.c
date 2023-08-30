5bd7d54a6a476bc2fd57a395efdea9906a02cfbd
Mbang Ernest N <mbangernest@yahoo.com>
2023-08-30 13:09:10 +0100

    fix: modify NULL value and ensure proper dependency availability

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,11 +11,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *newNode;
 
 	newNode = malloc(sizeof(binary_tree_t));
-	if (newNode == NUll)
+	if (newNode == NULL)
 		return (NULL);
 
 	newNode->parent = parent;
-	newNode->value = value;
+	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
 
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -31,6 +32,9 @@ typedef struct binary_tree_s avl_t;
 
 /*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
+
+/* print binary tree*/
+void binary_tree_print(const binary_tree_t *tree);
 
 /*create a node q0*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
